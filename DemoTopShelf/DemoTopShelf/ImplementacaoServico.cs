d1374bce8406a0308850babc4fc8a3e4df97b490
Fabiano Nalin <fnalin@msn.com>
2016-08-10 16:23:44 -0300

    Criado serviço teste

---
+++ DemoTopShelf/DemoTopShelf/ImplementacaoServico.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace DemoTopShelf
+{
+    public class ImplementacaoServico : ServiceControl
+    {
+        private readonly CancellationTokenSource _cancellationTokenSource;
+
+        public ImplementacaoServico()
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        public bool Start(HostControl hostControl)
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(TimeSpan.FromSeconds(5));
+                    Console.WriteLine("It is {0} and all is well", DateTime.Now);
+                }
+            }, _cancellationTokenSource.Token);
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            _cancellationTokenSource.Cancel();
+            _cancellationTokenSource.Dispose();
+            return true;
+        }
+    }
+}
---
+++ DemoTopShelf/DemoTopShelf/Program.cs
@@ -0,0 +1,26 @@
+﻿using Topshelf;
+
+namespace DemoTopShelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(configurator =>
+            {
+                configurator.Service<ImplementacaoServico>(s =>
+                {
+                    s.ConstructUsing(name => new ImplementacaoServico());
+                    s.WhenStarted((service, control) => service.Start(control));
+                    s.WhenStopped((service, control) => service.Stop(control));
+                });
+                configurator.RunAsLocalSystem();
+
+                //It is recommended that service names not contains spaces or other whitespace characters.
+                configurator.SetServiceName("MyServiceByTopshelf");
+                configurator.SetDescription("Sample Topshelf Host");
+                configurator.SetDisplayName("Stuff");
+            });
+        }
+    }
+}
